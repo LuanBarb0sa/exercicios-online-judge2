e1820711580f87cde9f875dcb1e07ac4178a8a21
LuanBarb0sa <luanlucas.r10@hotmail.com>
2022-09-12 22:55:54 -0300

    Atualizando repo

---
+++ src/exercicios/URI1079.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class URI1079 {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+    	int N = leitor.nextInt();
+    	for (int i = 0; i < N; i++) {
+    		double n1 = leitor.nextDouble();
+    		double n2 = leitor.nextDouble();
+    		double n3 = leitor.nextDouble();
+    		double media = ((n1*2) + (n2*3) + (n3*5)) / 10;
+    		System.out.println(String.format("%.1f", media));
+    		leitor.close();
+    	}
+	}
+
+	}
